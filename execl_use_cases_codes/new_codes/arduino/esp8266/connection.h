27b3be97ddbbd544e9b3bc7f0da5d0b9d04747e0
Bunoviske <brunofcarvalho1996@gmail.com>
2016-04-30 12:06:38 -0300

    Create connection.h

---
+++ execl_use_cases_codes/new_codes/arduino/esp8266/connection.h
@@ -0,0 +1,13 @@
+class Connection{
+  
+  public:
+  
+  void find_network(String, String, const int);
+  void set_mode(int, const int);
+  void get_ip(const int);
+  void set_multiple_connection(bool mult, const int timeout);
+  void create_connection(String cipstart, const int timeout, int connectionId);
+  void close_connection(String closeConnection, const int timeout, int connectionId);
+ 
+};
+
