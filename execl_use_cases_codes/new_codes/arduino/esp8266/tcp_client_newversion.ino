2aa5622b87409b40e175f4a54ace90c1f2913719
Bunoviske <brunofcarvalho1996@gmail.com>
2016-04-30 12:45:10 -0300

    Update tcp_client_newversion.ino

--- execl_use_cases_codes/new_codes/arduino/esp8266/tcp_client_newversion.ino
+++ execl_use_cases_codes/new_codes/arduino/esp8266/tcp_client_newversion.ino
@@ -25,30 +25,6 @@ void setup()
     
 }
 
-
-
- String sendCommand(String command, const int timeout)
-{
-    String response = "";
-    
-    esp8266.print(command); // send the read character to the esp8266
-    
-    long int time = millis();
-    
-    while( (time+timeout) > millis())
-    {
-      while(esp8266.available())
-      {
-        
-        // The esp has data so display its output to the serial window 
-        char c = esp8266.read(); // read the next character.
-        response+=c;
-      }  
-    }
-    Serial.print(response);
-    return response;
-}
-
 void loop()
 {
      String cipSend;
