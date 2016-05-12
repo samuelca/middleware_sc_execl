22e635c22faf63ea15a3513bd01fd0b86df5bdb6
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-11 23:35:59 -0300

    Update connection.h

--- execl_use_cases_codes/new_codes/arduino/esp8266/connection.h
+++ execl_use_cases_codes/new_codes/arduino/esp8266/connection.h
@@ -1,15 +1,61 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H  
+
 #include "AT.h"
 
 class Connection{
-  
+
+  private:
+
+   String Ipadress; 
+   int Modo;
+   String SSID;
+   String PSWD;
+   int Multipleconnection;
+   String Protocolo;
+   String serverip;
+   int connectionId;
+   int porta;
+   
+
   public:
+
+  Connection(String param1, String param2, int param3, int param4, String param5, String param6, int param7)
   
-  void find_network(String, String, const int);
-  void set_mode(int, const int);
-  void get_ip(const int);
-  void set_multiple_connection(bool mult, const int timeout);
-  void create_connection(String cipstart, const int timeout, int connectionId);
-  void close_connection(String closeConnection, const int timeout, int connectionId);
- 
+  {
+      SSID = param1;
+      PSWD = param2;
+      Modo = param3;
+      Multipleconnection = param4;
+      Protocolo = param5;
+      serverip = param6;
+      porta = param7;
+      connectionId = 0;
+      Ipadress = "";
+
+
+
+  }
+
+
+
+
+
+  void reset(const int timeout);
+  void find_network(const int timeout);
+  void set_mode(const int timeout);
+  void get_ip(const int timeout);
+  void set_multiple_connection(const int timeout);
+  void create_connection(const int timeout);
+  void close_connection(const int timeout);
+  int get_connection();
+  String get_ip();
+  int get_connectionId();
+  void set_connectionId(int valor);
+
+
+
 };
+
+#endif
 
