c1c125f178b077b9f0019b1d5727b9fae13abb7b
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-11 23:36:56 -0300

    Update AT.h

--- execl_use_cases_codes/new_codes/arduino/esp8266/AT.h
+++ execl_use_cases_codes/new_codes/arduino/esp8266/AT.h
@@ -1,3 +1,20 @@
+#ifndef AT_H
+#define AT_H  
 
-  String sendCommand(String command, const int timeout);
-  void ATgenerator();
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+
+class AT{
+
+  private:
+    SoftwareSerial * esp8266 = new SoftwareSerial(2,3);
+  
+  public:
+  
+    void initialize_serial();
+    String sendCommand(String command, const int timeout);
+    char* connected_client(int *tam);
+    
+};
+
+#endif
