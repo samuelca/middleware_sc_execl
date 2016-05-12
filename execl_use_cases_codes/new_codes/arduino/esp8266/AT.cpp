6d6478306027d5dfc9f4faa88bcac31938f76df9
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-11 23:37:25 -0300

    Update AT.cpp

--- execl_use_cases_codes/new_codes/arduino/esp8266/AT.cpp
+++ execl_use_cases_codes/new_codes/arduino/esp8266/AT.cpp
@@ -1,26 +1,52 @@
-String sendCommand(String command, const int timeout)
+#include "AT.h"
+
+
+void AT::initialize_serial(){
+  esp8266->begin(115200);
+}
+
+String AT::sendCommand(String command, const int timeout)
 {
    String response = "";
-
-   esp8266.print(command); // send the read character to the esp8266
+   esp8266->print(command);
 
    long int time = millis();
 
    while( (time+timeout) > millis())
    {
-     while(esp8266.available())
+     while(esp8266->available())
      {
-
-       // The esp has data so display its output to the serial window
-       char c = esp8266.read(); // read the next character.
+       
+       char c = esp8266->read();
        response+=c;
+       
      }
    }
+   
    Serial.print(response);
    return response;
 }
 
-void ATgenerator()
-{
-
+char* AT::connected_client(int *tam){
+  char aux[] = "-1";
+  if (esp8266->available()){
+       if (esp8266->find("+IPD,")){
+          int i=0;
+          while (esp8266->available()){
+            aux[i] = esp8266->read();
+            i++;
+         }
+         *tam = i;
+         return aux;
+       }
+       else{
+         return aux;
+       }
+       
+  }
+  else{
+    return aux;
+  }
 }
+
+
