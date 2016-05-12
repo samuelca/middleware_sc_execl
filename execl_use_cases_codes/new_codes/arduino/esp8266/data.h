ecb9ec223cb7a0cc73064847409e20056989d7be
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-11 23:34:52 -0300

    Update data.h

--- execl_use_cases_codes/new_codes/arduino/esp8266/data.h
+++ execl_use_cases_codes/new_codes/arduino/esp8266/data.h
@@ -1,8 +1,21 @@
+#ifndef DATA_H
+#define DATA_H
+
 #include "AT.h"
 
 class Data
 {
+  private:
+
+  String data_value;
+      
   public:
-  void data_warning(String cipSend, String data,const int timeout, int connectionId);
-  void send_data(String data, const int timeout);
+
+  Data();
+  String get_data();
+  void set_data(String data);
+  void data_warning(const int timeout, int connectionId);
+  void send_data(const int timeout);
 };
+
+#endif
