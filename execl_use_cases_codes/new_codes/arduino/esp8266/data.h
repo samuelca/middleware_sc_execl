da42731fa0eebcd274e3a2616e42d17509bb7cd7
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-28 14:28:39 -0300

    Update data.h

--- execl_use_cases_codes/new_codes/arduino/esp8266/data.h
+++ execl_use_cases_codes/new_codes/arduino/esp8266/data.h
@@ -2,20 +2,25 @@
 #define DATA_H
 
 #include "AT.h"
+#include <aJSON.h> 
 
 class Data
 {
   private:
-
   String data_value;
+  String jsonstring;
       
   public:
 
-  Data();
   String get_data();
   void set_data(String data);
   void data_warning(const int timeout, int connectionId);
   void send_data(const int timeout);
+  void create_http(String data, String nome, String serverip);
+  char* parseJson(char*);
+  String get_jsonstring();
+  void check_json(String);
+  
 };
 
 #endif
