9cadc2eead5138f52e37ed0ed4b238acce744300
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-28 14:35:05 -0300

    Update tcpserver_newversion.ino

--- execl_use_cases_codes/new_codes/arduino/esp8266/tcpserver_newversion.ino
+++ execl_use_cases_codes/new_codes/arduino/esp8266/tcpserver_newversion.ino
@@ -1,40 +1,37 @@
-#include "connection.h"
-#include "data.h"
+#include "settings.h"
 
-//#include "AT.h"
-//#include "settings.h"
-
-#define SSID dlink-9FC8
-#define PSWD xpqap4952
-#define porta 8000
-
-Connection * esp_server = new Connection(SSID, PSWD, 0, 1, SERVER, 0, porta);
-Data * dado = new Data();
 
 void setup()
 {
   Serial.begin(9600);
+  
+  //configura o modulo e conecta o mesmo na rede
   esp_server->get_connection();
-  Serial.print(esp_server->get_ip());
-
+  
+  //inicializa o servidor
+  esp_server->create_connection(2000);
 }
 
 
 void loop()
 {
-   int tam;
-   char requisicao[] = connected_client(&tam);
-   if (requisicao[0] =! -1){
-      set_connectionId(requisicao[0] - 48);
-      int i=0;
-      while (i<tam){
-        Serial.print(requisicao[i]);
-      }
-      //falta avaliar a requisicao
+   String requisicao;
+   //verifica se houve alguma requisicao para o servidor
+   requisicao = esp_server->connected_client();
+   
+   if (requisicao != "-1"){
+     
+     esp_server->set_connectionId(requisicao[0] - 48);     // connectionId é um inteiro, entao pela tabela ASCII subtrai 48
+     Serial.print(requisicao);
+     
+     //falta avaliar a requisicao
      String data = "CONEXAO OK";
+
+     //envio de dados
      dado->set_data(data);
      dado->data_warning(1000, esp_server->get_connectionId());
      dado->send_data(1000);
+     
      esp_server->close_connection(3000);
    }
 
