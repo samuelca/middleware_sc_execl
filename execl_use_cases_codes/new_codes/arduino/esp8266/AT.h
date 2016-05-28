07d83b279f26c7345efdaf87fbbbd380edf12055
Bunoviske <brunofcarvalho1996@gmail.com>
2016-05-28 14:31:12 -0300

    Update AT.h

--- execl_use_cases_codes/new_codes/arduino/esp8266/AT.h
+++ execl_use_cases_codes/new_codes/arduino/esp8266/AT.h
@@ -4,17 +4,11 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
 
-class AT{
-
-  private:
-    SoftwareSerial * esp8266 = new SoftwareSerial(2,3);
-  
-  public:
-  
+    //deve usar static para ser declarado apenas uma vez e nao perder suas propriedades, como o esp8266->begin().
+    static SoftwareSerial * esp8266 = new SoftwareSerial(2,3);
+    void ATgenerator(char[], char); 
     void initialize_serial();
     String sendCommand(String command, const int timeout);
-    char* connected_client(int *tam);
-    
-};
+    String client_available();
 
 #endif
