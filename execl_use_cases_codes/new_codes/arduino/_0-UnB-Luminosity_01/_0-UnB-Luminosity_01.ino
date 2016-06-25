bbbbc716d0b4f797f104ed60a045d4ef747d4a28
Cleinilton Fernandes <tommfernandes@gmail.com>
2016-06-25 08:50:52 -0300

    Add files via upload

---
+++ execl_use_cases_codes/new_codes/arduino/_0-UnB-Luminosity_01/_0-UnB-Luminosity_01.ino
@@ -0,0 +1,35 @@
+/* UnB-UioT - Luminosity sensor, if dark so it lights, else lamp continue in low state */
+
+#define ANALOG_SENSOR_PIN  A0 
+#define DIGITAL_SENSOR_PIN 7
+#define LEDPIN    13  // LED connected with the pin #
+
+int  digitalValue;  /* Last digital value from module */
+int  analogValue; /* Last analogical value from module */
+
+void setup()
+{
+  pinMode(LEDPIN, OUTPUT);
+  Serial.begin(9600);          // Enable the Serial data output
+}
+
+
+void loop()
+/* If the digital value is HIGH (which means that is dark) then the light comes ON */
+{
+  digitalValue = digitalRead(DIGITAL_SENSOR_PIN);  
+  if (digitalValue == HIGH)
+  {
+    digitalWrite(LEDPIN, HIGH);
+    Serial.println("Digital Signal = ON ");
+  }  
+  else
+  {
+    digitalWrite(LEDPIN, LOW);
+  }
+
+  analogValue = analogRead(ANALOG_SENSOR_PIN);  //Read the voltage from sensor
+  Serial.print("Analog Value (from 0 to 1023): "); 
+  Serial.println(analogValue,DEC);      // Send result to Serial Monitor
+  delay(500);
+}
---
+++ execl_use_cases_codes/new_codes/arduino/_0-UnB-Temperatura_Humidade_01/_0-UnB-Temperatura_Humidade_01.ino
@@ -0,0 +1,27 @@
+//UnB-UioT - Humidity and Temperature sensor
+
+#include <dht.h>
+
+#define dht_dpin A0 //A0 connected with the pin out, from the sensor
+
+dht DHT; 
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop()
+{
+  DHT.read11(dht_dpin); //Read information from the sensor
+  Serial.print("Umidade = ");
+  Serial.print(DHT.humidity);
+  Serial.print(" %  "); //space
+  Serial.print("Temperatura = ");
+  Serial.print(DHT.temperature); 
+  Serial.println(" Celsius  ");
+
+  //Measurements delay
+  delay(2000);  
+}
